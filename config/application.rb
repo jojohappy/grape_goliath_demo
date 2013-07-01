86056675410e2786a7bfb656b833864e94dc8c80
jojohappy <jojohappy@126.com>
2013-07-01 12:46:07 +0800

    demo finish

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,9 @@
 config['mongo'] = EventMachine::Synchrony::ConnectionPool.new(size: 20) do
-  conn = EM::Mongo::Connection.new('172.16.0.9', 27017, 1, {:reconnect_in => 1})
-  db = conn.db('test')
-  # conn = EM::Mongo::Connection.new('172.17.10.218', 30000, 1, {:reconnect_in => 1})
-  # db = conn.db('admin')
-  # db.authenticate('root', '123456')
+  #conn = EM::Mongo::Connection.new('172.16.0.9', 27017, 1, {:reconnect_in => 1})
+  #conn = EM::Mongo::Connection.new('172.17.10.137', 27017, 1, {:reconnect_in => 1})
+  conn = EM::Mongo::Connection.new('172.17.10.215', 27017, 1, {:reconnect_in => 1})
+  db = conn.db('admin')
+  #db.authenticate('root', '123456')
   db
 end
 
